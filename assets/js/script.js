c13de9b03eed20a5a3484e9ec3d7a319b92e799c
khimsh <i.khimsh@gmail.com>
2022-07-19 15:00:37 +0400

    markup almost finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
 ;(() => {
     'use strict'
 
-    const swiper = new Swiper('.hero__slider', {
+    const heroSwiper = new Swiper('.hero__slider', {
         pagination: {
             el: '.hero__pagination',
+            clickable: true,
+        },
+    })
+
+    const teamSwiper = new Swiper('.team__slider', {
+        slidesPerView: 3.2,
+        spaceBetween: 40,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
         },
     })
 })()

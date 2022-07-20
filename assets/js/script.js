869d5a777d561583f064807d93ba14612e5c244f
khimsh <i.khimsh@gmail.com>
2022-07-20 09:10:14 +0400

    mobile navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 ;(() => {
-    'use strict'
+    ;('use strict')
 
+    /************************
+        Sliders
+    *************************/
     const heroSwiper = new Swiper('.hero__slider', {
         pagination: {
             el: '.hero__pagination',
@@ -28,4 +31,41 @@
             },
         },
     })
+
+    /************************
+        Navigation
+    *************************/
+    function mobileNav() {
+        if (!document.querySelector('.site-header__btn')) return
+
+        const nav = document.querySelector('.site-nav')
+        const openNav = document.querySelector('.site-header__btn')
+        const links = nav.querySelectorAll('.site-nav__link')
+
+        openNav.addEventListener('click', () => {
+            nav.classList.toggle('active')
+
+            if (nav.classList.contains('active')) {
+                openNav.innerHTML = '<i class="fa-solid fa-xmark"></i>'
+            } else {
+                openNav.innerHTML = '<i class="fa-solid fa-bars"></i>'
+            }
+        })
+
+        links.forEach((link) => {
+            link.addEventListener('click', () => {
+                // let currentHref = window.location.href
+
+                links.forEach((link) => {
+                    link.classList.remove('active')
+                })
+
+                link.classList.add('active')
+                nav.classList.remove('active')
+                openNav.innerHTML = '<i class="fa-solid fa-bars"></i>'
+            })
+        })
+    }
+
+    mobileNav()
 })()

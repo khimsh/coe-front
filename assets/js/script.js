8e9d6a332a1c8249d36a8270f03f988714effcf3
khimsh <i.khimsh@gmail.com>
2022-07-20 08:49:23 +0400

    news article preview responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,11 +9,23 @@
     })
 
     const teamSwiper = new Swiper('.team__slider', {
-        slidesPerView: 3.2,
+        slidesPerView: 1.2,
         spaceBetween: 40,
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 2.2,
+                spaceBetween: 30,
+            },
+            // when window width is >= 768px
+            768: {
+                slidesPerView: 3.2,
+                spaceBetween: 40,
+            },
         },
     })
 })()
